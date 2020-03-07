f537fa38d975e081aa0ffc3632a1f3d9611c2f0d
duanmuchaoqun <2947824360@qq.com>
2020-03-07 23:21:05 +0800

    第二天课程提交，vue项目启动，后台属性查询

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 #gmall-user用户服务8080
 gmall-user-service用户服务的service层8070
 gmall-user-web用户服务的web层8080
+
+
+gmall-manage-service后台服务的service层8071
+gmall-manage-web后台服务的web层8081
---
+++ gmall-manage-web/src/main/java/com/atguigu/gmall/manage/controller/AttrController.java
@@ -0,0 +1,36 @@
+package com.atguigu.gmall.manage.controller;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.atguigu.gmall.bean.PmsBaseAttrInfo;
+import com.atguigu.gmall.service.AttrService;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+
+@CrossOrigin //跨域注解
+@Controller
+public class AttrController {
+
+    @Reference
+    AttrService attrService;
+
+    @RequestMapping("/saveAttrInfo")
+    @ResponseBody
+    public String saveAttrInfo(@RequestBody PmsBaseAttrInfo pmsBaseAttrInfo){
+        return "success";
+    }
+
+
+
+    @RequestMapping("/attrInfoList")
+    @ResponseBody
+    public List<PmsBaseAttrInfo> attrInfosList(String catalog3Id){
+        List<PmsBaseAttrInfo> attrInfosList =attrService.getAttrInfoList(catalog3Id);
+        return attrInfosList;
+    }
+
+}
